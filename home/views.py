14e7204d9e81843f97b9ed02576af8649358bbb4
nfepb <nicolas.planteb@gmail.com>
2023-02-12 12:47:07 +0000

    fix: styling navbar & link to account login/signup pages.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
 
+def home(request):
+    """
+    view to return the home page
+    """
+    return render(request, "home/home.html")
+
+
 def index(request):
     """
     View to return the index page
--- tourbillon/urls.py
+++ tourbillon/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls'), name="accounts.urls"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
