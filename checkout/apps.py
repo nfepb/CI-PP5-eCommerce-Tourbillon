85e48f05f89f8847d99c209aa89123a647d32c8c
nfepb <nicolas.planteb@gmail.com>
2023-02-24 20:36:05 +0000

    fix: fixes PyLint flagged errors in doctstrings.

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,9 +1,12 @@
+"""checkout app.py"""
 from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
+    """App config for checkout app"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
 
     def ready(self):
+        """checkout signals"""
         import checkout.signals
