85c07efa3bfb900fe41f23fab4e112bf502cb2c9
nfepb <nicolas.planteb@gmail.com>
2023-02-25 11:28:27 +0000

    feat: Adds checkout form for users to input shipping info for order.

---
+++ checkout/forms.py
@@ -0,0 +1,53 @@
+"""checkout/forms.py for users to fill in shipping info for Order. 
+Credit: Code Institute, Boutique Ado project
+"""
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    """
+    Creates the users order form information & field association
+    """
+    class Meta:
+        """Change the behaviour of Order model fields"""
+        model = Order
+        fields = (
+            "full_name",
+            "email",
+            "phone_number",
+            "street_address1",
+            "street_address2",
+            "town_or_city",
+            "postcode",
+            "country",
+            "county",
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Adds placeholders and classes, removes auto-generated
+        labels and sets autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            "full_name": "Full Name",
+            "email": "Email Address",
+            "phone_number": "Phone Number",
+            "postcode": "Postal Code",
+            "town_or_city": "Town or City",
+            "street_address1": "Street Address 1",
+            "street_address2": "Street Address 2",
+            "county": "County, State or Locality",
+        }
+
+        self.fields["full_name"].widget.attrs["autofocus"] = True
+        for field in self.fields:
+            if field != "country":
+                if self.fields[field].required:
+                    placeholder = f"{placeholders[field]} *"
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs["placeholder"] = placeholder
+            self.fields[field].widget.attrs["class"] = "stripe-style-input"
+            self.fields[field].label = False
