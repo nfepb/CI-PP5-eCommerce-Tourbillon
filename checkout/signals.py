8b9c6ef5cde6c7896a9074c23399fc254b3124f2
nfepb <nicolas.planteb@gmail.com>
2023-02-26 10:42:42 +0000

    fix: configures django-pylint to load plugin and disregard C0114, C0115, W0222 errors.

--- checkout/signals.py
+++ checkout/signals.py
@@ -5,7 +5,7 @@ from .models import OrderLineItem
 
 
 @receiver(post_save, sender=OrderLineItem)
-def update_on_save(sender, instance, created, **kwargs):
+def update_on_save(sender, instance, **kwargs):
     """
     Updates order total on lineitem create/update.
     sender = OrderLineItem
