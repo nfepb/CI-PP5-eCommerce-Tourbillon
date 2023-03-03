4975f4753a5d6b4d0458f2c8c8cf0a9cf4c18c6b
nfepb <nicolas.planteb@gmail.com>
2023-03-03 18:02:26 +0000

    feat: Adds product form to allow management of products.

---
+++ products/templates/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
+from .widgets import CustomClearableFileInput
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+    """
+    Creates the Product form information
+    """
+    class Meta:
+        model = Product
+        # Include all the fields
+        fields = "__all__"
+        widgets = {
+            "description": SummernoteWidget(),
+            "watch_details": SummernoteWidget(),
+            "features": SummernoteWidget(),
+        }
+
+    image = forms.ImageField(
+        label="Image", required=False, widget=CustomClearableFileInput
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Override init method to make a few changes to fields
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        # List comprehension associating friendly name to category ID.
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields["category"].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "border-secondary rounded-1"
