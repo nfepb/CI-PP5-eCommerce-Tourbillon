41192a8dfa31e80e69dd87aeeab73d1b2df6d86a
nfepb <nicolas.planteb@gmail.com>
2023-03-03 17:11:39 +0000

    feat: Adds comment in profiles/countryfields.js

--- profiles/static/profiles/js/countryfields.js
+++ profiles/static/profiles/js/countryfields.js
@@ -1,7 +1,10 @@
+// Get value of country field on page load and store in variable
 let countrySelected = $("#id_default_country").val();
+// Empty var if not selected --> if not selected, display in color:
 if (!countrySelected) {
   $("#id_default_country").css("color", "#aab7c4");
 }
+// Capture change event every time box change, capture value & determine color
 $("#id_default_country").change(function () {
   countrySelected = $(this).val();
   if (!countrySelected) {
