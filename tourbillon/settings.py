d4de2d9058baff3fb81e5347d0ddb084bd5d01c7
nfepb <nicolas.planteb@gmail.com>
2023-03-05 11:50:57 +0000

    feat: heroku deployment

--- tourbillon/settings.py
+++ tourbillon/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-886%g0j2g7$9=x3qlop0)2tk4)8%@&ql6#5#nxnqo-f!xqtfi*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pp5-ecommerce-tourbillon.herokuapp.com', 'localhost']
 
 
 # Application definition
